63ca8028345e83954bd82e83ad274e2bbe8cc51f
higan96 <norihiko.oba@gmail.com>
2020-03-14 20:39:24 +0900

    ダミーデータ作成をもう少しましに

--- MovieTicket/ProgramRepository.swift
+++ MovieTicket/ProgramRepository.swift
@@ -14,27 +14,70 @@ protocol ProgramRepository {
 
 struct ProgramRepositoryMock: ProgramRepository {
     func programCollections() -> [ProgramCollection] {
-        let movie = Movie(name: "クレヨンしんちゃん")
-        let theater = Theater(name: "シアターA", sheets: sheets(count: 50))
-        let shcedule = Schedule(startAt: Date(), endAt: Date())
-        let program = Program(movie: movie, theater: theater, schedule: shcedule)
-        let collection = ProgramCollection(movie: movie, programs: [program])
-        
-        return [collection]
+        let movies = self.movies()
+        return movies.map { ProgramCollection(movie: $0, programs: self.progras(movie: $0)) }
     }
     
     func sheets(count: Int) -> [Sheet] {
-        let alphabetLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
         var sheets: [Sheet] = []
         
-        for i in 1...count {
-            let alphabet = alphabetLetters[i / 10]
-            let sheetLabel = "\(alphabet)-\(String(i))"
-            
-            let sheet = Sheet(label: sheetLabel, status: .empty)
+        for i in 0...count {
+            let sheet = Sheet(label: i.toSheetLabel(), status: .empty)
             sheets.append(sheet)
         }
         
         return sheets
+    }
+    
+    func progras(movie: Movie) -> [Program] {
+        var programs: [Program] = []
+        
+        let theaters = self.theaters(count: 10)
+        for _ in 0...5 {
+            let program = Program(movie: movie, theater: theaters.randomElement()!, schedule: Schedule(startAt: Date(), endAt: Date()))
+            programs.append(program)
+        }
+        
+        return programs
+    }
+    
+    func movies() -> [Movie] {
+        return [
+            Movie(name: "StarWars episode1"),
+            Movie(name: "StarWars episode2"),
+            Movie(name: "StarWars episode3"),
+            Movie(name: "StarWars episode4"),
+            Movie(name: "StarWars episode5"),
+            Movie(name: "StarWars episode6"),
+            Movie(name: "StarWars episode7"),
+            Movie(name: "StarWars episode8"),
+            Movie(name: "StarWars episode9"),
+            Movie(name: "クレヨンしんちゃん"),
+            Movie(name: "かいけつゾロリ"),
+            Movie(name: "忍たま乱太郎"),
+        ]
+    }
+    
+    func theaters(count: Int) -> [Theater] {
+        var theaters: [Theater] = []
+        
+        for i in 0...count {
+            let name = "シアター\(i + 1)"
+            let theater = Theater(name: name , sheets: sheets(count: 50))
+            theaters.append(theater)
+        }
+        
+        return theaters
+    }
+}
+
+private extension Int {
+    func toSheetLabel() -> String {
+        let alphabetLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        let alphabet = alphabetLetters[self / 10]
+        let number = self % 10 + 1
+        let formattedNumber = number < 10 ? "0" + String(number) : String(number)
+        
+        return  "\(alphabet)-\(formattedNumber)"
     }
 }
